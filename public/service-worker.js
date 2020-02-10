731ca2b985af54a0bad5cd75f2a227a1139bd34a
mattchley <mattchley@gmail.com>
2020-02-10 11:29:51 -0800

    worksOffline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 //Your code goes here
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "styles.css"];
+const FILES_TO_CACHE = ["/", "/index.html", "index.js", "styles.css", "db.js"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
